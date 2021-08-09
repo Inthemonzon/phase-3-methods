a9c97b46017e65a31b80a4c07da5e3ca9dbc5568
inthemonzon <inthemonzon@yahoo.com>
2021-08-08 23:32:17 -0400

    commit tree 0224b0e0d18d0db051c039e485b3b2dc9f6f88d0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
-# Your code here!
+# JavaScript Function:
+
+#   function myFunction(param) {
+#     console.log("Running myFunction");
+#     return param + 1;
+# }
+
+# const myFunctionReturnValue = myFunction(1);
+# console.log(myFunctionReturnValue);
+
+#Ruby Method(Method is ruby equivalent to JavaScript function):
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "John")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "John")
+        puts "Hello, #{name}!"
+        puts "Hello, programmer!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer 
+        return nil;
+    end
+
+    return num/2
+end
